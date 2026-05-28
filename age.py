2567c46405c80d66042b9117b6296a9f7a5ee9b2
Barteli <arthurbarteli27@gmail.com>
2026-05-28 19:26:49 -0400

    Iniciando age.py

--- age.py
+++ age.py
@@ -8,9 +8,12 @@ mes = int(5)
 nascimento_y = ano - idade
 nascimento_m = mes - nasceu_m
 
+
 print ("Seu nome é " + name)
 print ("Você tem", idade, "anos")
 if nasceu_m <= mes:
     print(nascimento_y)
 else :
     print("Você nasceu em", nascimento_y - 1)
+
+    print("Você tem", nascimento_m, "meses")
