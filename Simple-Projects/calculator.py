ea3dc8925549b077f53bdfa74c602471fb062c57
Barteli <arthurbarteli27@gmail.com>
2026-06-01 02:13:13 -0400

    Organizando estrutura do projeto

--- Simple-Projects/calculator.py
+++ Simple-Projects/calculator.py
@@ -1,15 +1,15 @@
 while True:
-    operacao = input("Qual é a operação que você deseja fazer? multiplicacao, subtracao, soma ou divisao? ")
+    operacao = input("Qual é a operação que você deseja fazer? (x, +, -, /) ")
     num1 = float(input("Digite um numero "))
     num2 = float(input("Digite outro numero "))
 
-    if operacao == "multiplicacao":
+    if operacao == "x":
         print(num1 * num2)
-    elif operacao == "soma":
+    elif operacao == "+":
         print(num1 + num2)
-    elif operacao == "subtracao":
+    elif operacao == "-":
         print(num1 - num2)
-    elif operacao == "divisao":
+    elif operacao == "/":
         print(num1 / num2)
     else:
         print("Operação invalida! (tente sem acentos)")
